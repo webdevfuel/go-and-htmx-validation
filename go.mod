a7a6b333e80f0901b7a0d17440d55939fcaa0860
Emanuel Turis <hello@emanuelturis.com>
2024-10-12 17:21:15 +0300

    validate form with map

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/webdevfuel/go-and-htmx-validation
 
 go 1.23.2
 
-require github.com/a-h/templ v0.2.778 // indirect
+require (
+	github.com/a-h/templ v0.2.778
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
+)
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/webdevfuel/go-and-htmx-validation/template"
 	"github.com/webdevfuel/go-and-htmx-validation/user"
 )
@@ -16,15 +17,48 @@ func main() {
 		component.Render(r.Context(), w)
 	})
 	r.HandleFunc("POST /users", func(w http.ResponseWriter, r *http.Request) {
-		u := user.User{
-			ID:   r.FormValue("id"),
-			Name: r.FormValue("name"),
+		// parse form
+		err := r.ParseForm()
+		if err != nil {
+			renderFormComponent(w, r)
+			return
 		}
+		// put values into map
+		values := make(map[string]string)
+		for k, v := range r.PostForm {
+			values[k] = v[0]
+		}
+		// validate map
+		err = validation.Validate(values,
+			validation.Map(
+				validation.Key("id", validation.Required, validation.Length(8, 0)),
+				validation.Key("name", validation.Required),
+			),
+		)
+		if err != nil {
+			renderFormComponent(w, r)
+			return
+		}
+		// construct user
+		u := user.User{
+			ID:   values["id"],
+			Name: values["name"],
+		}
+		// update slice of users
 		user.AddUser(u)
-		userComponent := template.User(u)
-		userComponent.Render(r.Context(), w)
-		formComponent := template.Form()
-		formComponent.Render(r.Context(), w)
+		// render form and user
+		renderFormComponent(w, r)
+		renderUserComponent(w, r, u)
 	})
 	http.ListenAndServe("localhost:3000", r)
+}
+
+func renderFormComponent(w http.ResponseWriter, r *http.Request) {
+	formComponent := template.Form()
+	formComponent.Render(r.Context(), w)
+}
+
+func renderUserComponent(w http.ResponseWriter, r *http.Request, u user.User) {
+	userComponent := template.User(u)
+	userComponent.Render(r.Context(), w)
 }
